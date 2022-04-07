7635a6b6a78a648fc6276e11c738cb966596a797
UwU <9334746+the-robot@users.noreply.github.com>
2022-04-08 00:13:27 +0800

    Create wezterm.lua

---
+++ terminals/wezterm/wezterm.lua
@@ -0,0 +1,17 @@
+local wezterm = require 'wezterm';
+
+local config = {
+  check_for_updates = true,
+
+  text_background_opacity = 1,
+  window_background_opacity = 0.98,
+
+  -- font
+  font = wezterm.font("JetBrains Mono", {weight="Bold", italic=false}),
+  font_size = 10.0,
+
+  -- color schemes
+  color_scheme = "Andromeda",
+}
+
+return config
